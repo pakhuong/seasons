3b58c8bfb6f8c0c7feb4a01d65d6bec435d8a456
pakhuong <anhkhuong1602@gmail.com>
2018-12-30 23:00:22 +0700

    Implement getSeason function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import SeasonDisplay from './SeasonDisplay';
+
 class App extends Component {
     state = { lat: null, errorMessage: '' };
 
@@ -17,7 +19,7 @@ class App extends Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
         return <div>Loading!</div>;

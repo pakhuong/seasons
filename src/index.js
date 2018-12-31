b17da9a4f6b3a5ad3d893739ac6bb5dd1388ee9c
pakhuong <anhkhuong1602@gmail.com>
2018-12-31 15:59:31 +0700

    Extract the conditional rendering to a separate function

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class App extends Component {
         );
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -24,6 +24,10 @@ class App extends Component {
         }
 
         return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return this.renderContent();
     }
 }
 
